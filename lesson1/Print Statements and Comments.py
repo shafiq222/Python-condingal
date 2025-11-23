6e4552e3582ffd58b34c7fdafd4fad45fd35d244
shafiq222 <supermarketazaan@gmail.com>
2025-11-23 20:54:01 +0300

    lesson1

---
+++ lesson1/Print Statements and Comments.py
@@ -0,0 +1,11 @@
+# print a greeting message
+print("Hello world")
+
+# print my name
+print("My name is Azaan")
+
+# fun fact about me 
+print("I like skateoarding")
+
+# Math work
+print("3+9=",3+9)

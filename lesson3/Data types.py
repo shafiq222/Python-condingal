3da7031e4339b92bf242cdefa20285ddcde4769f
shafiq222 <supermarketazaan@gmail.com>
2025-12-07 20:57:46 +0300

    lesson3

---
+++ lesson3/Data types.py
@@ -0,0 +1,11 @@
+a=5
+print("print a",type(a))
+
+b=2.5
+print("print b",type(b))
+
+c="coding"
+print("print c",type(c))
+
+d=True
+print("print d",type(d))
---
+++ lesson3/Typecasting.py
@@ -0,0 +1,20 @@
+name = "Penguin"
+age=15
+is_student=True
+weight=39.2
+print("Name:",name)
+print(" Data type of name:",type(name))
+print("Age:",age)
+print(" Data type of age:",type(age))
+print("Is_student",is_student)
+print(" Data type of is_student:",type(is_student))
+print("Weight:",weight)
+print(" Data type of weight:",type(weight))
+print("\n After type casting .........")
+age=str(age)
+print(age)
+print(" Data type of age is:",type(age))
+weight=int(weight)
+print(weight)
+print(" Data type of weight:",type(weight))
+

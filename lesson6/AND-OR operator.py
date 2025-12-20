dfaab3e6247d268a4f245279664f8b8a9be1a11e
shafiq222 <supermarketazaan@gmail.com>
2025-12-20 21:04:39 +0300

    lesson6

---
+++ lesson6/AND-OR operator.py
@@ -0,0 +1,22 @@
+m=12
+a=12
+s=[]
+
+if m and a and s:
+    print("all the numbers hove boolean true")
+else:
+    print("one of the value is flase")
+
+b=10
+c=-10
+d=0
+
+if b>0 or c>0:
+    print("All No. are greater than 0 ")
+else:
+    print("No No. is greater than 0")
+
+if c>0 or d>0:
+     print("all the numbers hove boolean true")
+else:
+    print("one of the value is flase")
---
+++ lesson6/NOT Equal Operato.py
@@ -0,0 +1,12 @@
+a=10
+b=-12
+c=-12
+print(a !=b)
+print(b !=c)
+a1="azaan"
+b2="teacher"
+if a1 != b2:
+    print(a,"and",b ,"are diffrent")     
+a2=int(input("enter a number"))
+if a2%2 !=0:
+    print(a2,"it is an odd number")

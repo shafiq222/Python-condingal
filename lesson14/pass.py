9fff68d90dcb4f77276630753fcbc9c76934aec7
shafiq222 <supermarketazaan@gmail.com>
2026-01-18 20:58:36 +0300

    lesson14

---
+++ lesson14/pass.py
@@ -0,0 +1,11 @@
+for i in range(20):
+    if i % 20 == 0 :
+        print("Twist")
+    elif i % 15 ==0:
+        pass
+    elif i % 5 ==0:
+        print("Fizz")
+    elif i % 3 ==0:
+        print("Buzz")
+    else:
+        print (i)

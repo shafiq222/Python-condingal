71f446a493ef66aed3a3defbad791022df00cd34
shafiq222 <supermarketazaan@gmail.com>
2026-03-15 21:03:18 +0300

    lesson30

---
+++ lesson30/OverloadOperaters.py
@@ -0,0 +1,18 @@
+class A:
+    def __init__(self,a):
+        self.a = a
+    def __lt__(self,other):
+        if(self.a<other.a):
+            return "ob1 is less than ob2"
+        else:
+            return "ob2 is is less ob1"
+    def __eq__(self,other):
+        if(self.a==other.a):
+            return "Both are equal"
+        else:
+            return "Not equal"
+ob1 = A(2)
+ob2 = A(3)
+print("Passed Values :", ob1.a, ob2.a)
+print(ob1 < ob2)
+print(ob1 == ob2)

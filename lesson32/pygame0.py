3fa19c77b7460e72aa74b9b82fc1156810573cfd
shafiq222 <supermarketazaan@gmail.com>
2026-03-28 20:59:38 +0300

    lesson31,32

---
+++ lesson32/pygame0.py
@@ -0,0 +1,12 @@
+import pygame  
+pygame.init()
+screen = pygame.display.set_mode((400,300))
+done = False
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+    pygame.draw.rect(screen,pygame.Color("blue"),pygame.Rect(30,30,60,60))
+
+    pygame.display.flip()
